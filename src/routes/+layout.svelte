4efb269a19e8aa5ca3ae048b8bb96fb4ed976466
sanko <sang.koh@hotmail.com>
2023-12-18 19:38:00 +1100

    add page transitions

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+	import {fade} from "svelte/transition";
+
+	export let data;
+	$: pathname = data.pathname;
+
+	const fadeIn = {
+		x: 20,
+		duration: 200,
+		delay: 300,
+	};
+	const fadeOut = {
+		x: -20,
+		duration: 200,
+	};
+
+</script>
+
+{#key pathname}
+<div in:fade={fadeIn} out:fade={fadeOut}>
+	<slot/>
+</div>
+{/key}
